09e0e18b0b0b8486dab45ec9af0d4f12ed7f483b
jennifer <jennifer.yreyesgarcia@gmail.com>
2024-10-16 14:26:07 -0600

    creación del login

--- login.php
+++ login.php
@@ -3,9 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>login</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
+    <header>
+    <nav id="menu" >
+            
+            <div class="menu-bar">
+
+            <ul>
+                
+                <li>
+                    <a href="#">
+                        Inicio
+                    </a>
+                    
+                </li>
+                
+                <li><a href="#">Saber más</a></li>
+                <li><a href="#">Otro</a></li>
+                
+                
+                
+
+            </ul>
+
+            </div>
+        </nav>
+    </header>
     
+
+    <div class="seccion-login">
+        
+            
+            <form action="" method="post">
+            <div class="titulo">
+                <h1>Iniciar sesión</h1>
+            <div class="linea"></div>
+            </div>    
+            
+            <div class="inputs">
+                <label for="nombre">Nombre de usuario</label>
+                <input type="text" name="usuario">
+            </div>
+            <div class="inputs">
+                <label for="password">Contraseña</label>
+                <input type="password" name="password">
+            </div>    
+                
+                <div class="boton-cont">
+                    <input type="submit" value="Iniciar sesión" class="boton">
+                </div>
+
+                <div class="registrarse">
+                    <p>¿No tienes una cuenta? <a href="#">Registrarse</a> </p>
+                </div>
+            </form>
+        
+        
+    </div>
 </body>
 </html>
