2a7779cedf03fd4f867de5de45cae5fc795ec5ee
ledcy <emmanuelriveramunoz29@gmail.com>
2024-10-22 08:20:00 -0600

    version 1.2
    Paginas principales Muestra de avance

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-
+    
         <!-- Bootstrap CSS v5.2.1 -->
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
@@ -17,40 +17,60 @@
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="templatess/headerStyle.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
+<nav>
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+      <img src="imagenes/LogoDinamiCash.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      DinamiCash
+    </a>
+  </div>
+</nav>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#"><img class="Imagen"src="imagenes/LogoDinamiCash.png" alt=""><span class="nombreLogo">DinamiCash</span></a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarScroll">
-                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Calendario</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Acciones
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Registrar compra</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Promedio</a></li>
-                    </ul>
-                  </li>
-                </ul>
-                <form class="d-flex" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-info" type="submit">Search</button>
-                </form>
-              </div>
-            </div>
-          </nav>
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarScroll">
+    <ul
+        class="nav justify-content-center  "
+    >
+
+    <li>
+    <i class='bx bxs-user-circle bx-sm'>Nombre</i>
+    </li>
+    </ul>
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Acciones
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="promedio.php">Promedia</a></li>
+            <li><a class="dropdown-item" href="calendario.php">Calendario</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="sueños.php">Cumple tus objetivos</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</nav>
+
+<nav class="navbar bg-primary" data-bs-theme="dark">
+</nav>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="funcions.js"></script>
+
 </html>
--- index.php
+++ index.php
@@ -2,16 +2,45 @@
 
 include("templatess/header.php")
 
-
 ?>
-    <div class="container">
-                <div class="background"></div>
-                <div class="content">
-                    <div class="trophy"><img src="imagenes/logoDinamiCash.png" alt=""></div>
-                    <h1>DinamiCash la nueva forma de hacer rendir tu dinero</h1>
-                    <p>Como una verdadera aplicación que hará cumplir todo aquello que te apasiona asi como tus sueños y metas.</p>
-                    <div class="boton">
-                    <button type="button" class="btn btn-primary btn-sm">Conoce mas de la aplicación</button>
-                    </div>
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<link rel="stylesheet" href="style.css">
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<div class="banner">
+        <div class="banner-content">
+            <h1>Guiar hacia un  ahorro significativo de DINERO </h1>
+            <a href="#" class="btn">Ver más</a>
+        </div>
+</div>
+<nav class="navbar bg-primary" data-bs-theme="dark">
+    
+</nav>
+<section class="banner2">
+    <div class="texto">
+    <h1><i class='bx bxs-food-menu bx-sm'></i>Conoce DinamiCash</h1>
+    </div>
+    <p># UNETE A NOSTROS AHORA MISMO</p>
+
+</section>
+<nav class="navbar bg-primary" data-bs-theme="dark">  
+</nav>
+
+</br>
+    
+    <div class="programs">
+        <div class="program">
+            <img src="imagenes/administrar.jpeg" alt="Estudiante trabajando en un proyecto artístico">
+            <div class="program-content">
+                <h2>Administra</h2>
+                <p>Aprende de la mejores funciones de ahorro con DinamiCash</p>
             </div>
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        </div>
+        
+        <div class="program">
+            <img src="imagenes/programar.png" alt="Estudiantes colaborando en un taller">
+            <div class="program-content">
+                <h2>Programa</h2>
+                <p>Desarróllate profesionalmente obteniendo un posgrado o especialidad que te ayudará a tener una ventaja competitiva en el mercado laboral.</p>
+            </div>
+        </div>
+    </div>
